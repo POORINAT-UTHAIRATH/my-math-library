5375b4436bdcfe2b6d5fbcb6290bdddc2010c746
POORINAT.UTH <poorinat.uth@dome.tu.ac.th>
2021-02-08 16:05:19 +0700

    Refactor code

--- main.test.js
+++ main.test.js
@@ -1,8 +1,8 @@
 const math = require('./main')
 const assert = require('assert')
 
-// console.log(math.sum(2, 2))
+console.log(math.sum(2, 2))
 assert.strictEqual(math.sum(2, 2), 4, 'should sum two numbers')
+console.log(math.sum([1, 2, 3, 4, 5]))
 assert.strictEqual(math.sum([1, 2, 3, 4, 5]), 15, 'should sum two numbers')
 
-console.log(math.sum([1, 2, 3, 4, 5]))
