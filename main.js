a48424a56a7da577006a7836da39925e1c6a12b5
POORINAT.UTH <poorinat.uth@dome.tu.ac.th>
2021-02-08 16:10:08 +0700

    Refactor to use array.reduce

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function sum(a, b){
     if (Array.isArray(a)) {
-        let total = 0
-        a.forEach((i) => (total += i))
-        // for (let i=0; i<a.length ; i++)
-        //     total += a[i]
-        return total
+        return a.reduce((total, i) => total + i, 0)
     }
     return a + b
 }
