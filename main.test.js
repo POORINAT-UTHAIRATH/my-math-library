ffd2cb38d229662e09c951b041466f790e68aca3
POORINAT.UTH <poorinat.uth@dome.tu.ac.th>
2021-02-08 16:01:47 +0700

    add sum array functionality

--- main.test.js
+++ main.test.js
@@ -3,3 +3,6 @@ const assert = require('assert')
 
 // console.log(math.sum(2, 2))
 assert.strictEqual(math.sum(2, 2), 4, 'should sum two numbers')
+assert.strictEqual(math.sum([1, 2, 3, 4, 5]), 15, 'should sum two numbers')
+
+console.log(math.sum([1, 2, 3, 4, 5]))
